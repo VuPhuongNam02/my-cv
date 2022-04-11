d47cc9ed9e9cacd60c2ccb94c91c0bd390910ea2
VuPhuongNam02 <vuphuongnam25122002@gmail.com>
2022-04-11 22:47:01 +0700

    schools  function

--- src/app/admin/schools/schools.component.ts
+++ src/app/admin/schools/schools.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { swal, swalMessage } from 'src/app/components/alert';
+import { SchoolsService } from 'src/app/service/schools/schools.service';
 
 @Component({
   selector: 'app-schools',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  school: any = {
+    id: 0,
+    name: '',
+    time_start: '',
+    time_end: '',
+    achievement: ''
   }
 
+  schools: any
+  isEdit: any = false
+  schoolForm: FormGroup = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    time_start: new FormControl('', Validators.required),
+    time_end: new FormControl('', Validators.required),
+    achievement: new FormControl('', Validators.required),
+  })
+
+  constructor(
+    private schoolService: SchoolsService
+  ) {
+    console.log(this.school.id);
+  }
+
+  ngOnInit(): void {
+    this.listschool()
+  }
+
+  listschool() {
+    this.schoolService.getSchools().subscribe(data => {
+      this.schools = data
+    })
+  }
+
+  submit(data: any) {
+    if (this.school.id == 0) {
+      swalMessage('Do you want to add school?', 'OK', 'Cancel').then((result) => {
+        if (result.isConfirmed) {
+          this.schoolService.createSchool(data).subscribe(() => {
+            this.listschool()
+            swal('Add school', 'You add school successfully !', 'success')
+            this.schoolForm.reset()
+          })
+        }
+      })
+    } else {
+      swalMessage('Do you want to save the changes?', 'Save', `Don't save`).then((result) => {
+        if (result.isConfirmed) {
+          this.schoolService.updateSchool(this.school.id, data).subscribe(() => {
+            this.listschool()
+            swal('Update', 'You updated school successfully', 'success')
+            location.reload()
+          })
+        }
+      })
+    }
+  }
+  edit(id: string | number) {
+    this.isEdit = true
+    this.schoolService.getSchoolById(id).subscribe(data => {
+      this.school = data
+    })
+  }
+
+  delete(id: string | number) {
+    swalMessage('Do you want to delete school?', 'OK', `Cancel`).then((result) => {
+      if (result.isConfirmed) {
+        this.schoolService.deleteSchool(id).subscribe(data => {
+          this.listschool()
+          swal('Delete', 'You deleted school successfully', 'success')
+        })
+      }
+    })
+  }
 }
--- src/app/service/schools/schools.service.ts
+++ src/app/service/schools/schools.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+const url = "http://localhost:3000/schools"
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getSchools(): Observable<any> {
+    return this.http.get(url)
+  }
+
+  createSchool(data: any): Observable<any> {
+    return this.http.post(url, data)
+  }
+
+  getSchoolById(id: string | number): Observable<any> {
+    return this.http.get(`${url}/${id}`)
+  }
+
+  updateSchool(id: string | number, data: any): Observable<any> {
+    return this.http.put(`${url}/${id}`, data)
+  }
+
+  deleteSchool(id: string | number): Observable<any> {
+    return this.http.delete(`${url}/${id}`)
+  }
 }
